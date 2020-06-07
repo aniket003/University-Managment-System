713151e1b157a8e728973bacfe22600fd2d72e33
aniket003 <60145059+aniket003@users.noreply.github.com>
2020-06-07 13:21:07 +0530

    Update AboutUs.java

--- University Management System/src/University/Management/System/AboutUs.java
+++ University Management System/src/University/Management/System/AboutUs.java
@@ -31,7 +31,7 @@ public class AboutUs extends JFrame {
 		l5.setForeground(Color.RED);
 		le.add(l5);
 		
-		l2=new JLabel("CREATORS");
+		l2=new JLabel("CREATOR");
 		l2.setFont(new Font("TimesRoman",Font.BOLD,35));
 		l2.setForeground(Color.BLACK);
 		l2.setBounds(200,20,500,30);
@@ -49,51 +49,9 @@ public class AboutUs extends JFrame {
         l4.setIcon(new ImageIcon(aa));
         
         le.add(l4);
-        l5=new JLabel("(Team Leader)");
-		l5.setFont(new Font("serif",Font.BOLD,14));
-		l5.setBounds(50,138,500,30);
-		le.add(l5);
-
-        		
-        l6=new JLabel("* Amandeep Sachdeva 1710991077 6G-01");
-		l6.setFont(new Font("serif",Font.BOLD,21));
-		l6.setBounds(20,240,500,30);
-		le.add(l6);
-        
-        ImageIcon aman=new ImageIcon("icons/aamm.jpg");
-        Image am=aman.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-        l7=new JLabel();
-        l7.setBounds(430,200,100,100);
-        l7.setIcon(new ImageIcon(am));
-        le.add(l7);
-        
-        
-        l8=new JLabel("* Anirudh Sharma 1710991100 6F-01");
-		l8.setFont(new Font("serif",Font.BOLD,24));
-		l8.setBounds(20,360,500,30);
-		le.add(l8);
         
 
-        ImageIcon ann=new ImageIcon("icons/anirudh.jpg");
-        Image agi=ann.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-        l9=new JLabel();
-        l9.setBounds(430,330,100,100);
-        l9.setIcon(new ImageIcon(agi));
-        le.add(l9);
-
-        l10=new JLabel("* Amod T.V 1710991081 6F-01");
-		l10.setFont(new Font("serif",Font.BOLD,24));
-		l10.setBounds(20,480,500,30);
-		le.add(l10);
-
-		 ImageIcon gg=new ImageIcon("icons/amod.jpg");
-	     Image qwe=gg.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-	     l11=new JLabel();
-	     l11.setBounds(430,460,100,100);
-	     l11.setIcon(new ImageIcon(qwe));
-	     le.add(l11);
-		
-        
+       
 	}
     public static void main(String [] agrs)
     {
